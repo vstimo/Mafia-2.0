724e610ed64239d11d7c2203c6e9a01120a0e992
Morar Timotei <126580714+vstimo@users.noreply.github.com>
2023-12-16 09:56:45 +0200

    Delete Mafia directory

--- Mafia/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import gui.InGameGUI;
-
-public class Main {
-    public static void main(String[] args){
-            new InGameGUI();
-    }
-}
--- Mafia/src/main/java/simplechat1/common/ChatIF.java
+++
@@ -1,6 +0,0 @@
-package simplechat1.common;
-
-public interface ChatIF 
-{
-  public abstract void display(String message);
-}
